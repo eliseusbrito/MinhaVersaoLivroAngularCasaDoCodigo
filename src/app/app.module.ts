d99b63d1bebd7eaf1a72c094644d95eac5a7aab1
eliseusbrito <eliseusbrito@gmail.com>
2021-08-30 20:03:30 -0300

    Criado componente ng g c lista-pessoa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ListaPessoaComponent } from './lista-pessoa/lista-pessoa.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListaPessoaComponent
   ],
   imports: [
     BrowserModule
